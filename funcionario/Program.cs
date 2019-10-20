dc873ef189fad86af2542ef69b7a345f052ed1c6
SthefanneBatista <estudanteb73@gmail.com>
2019-10-20 16:16:58 -0200

    Registro de funcionários

---
+++ funcionario/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcionario
+{
+    class Program
+    {
+         class Funcionario
+        {
+            private string v;
+            private string nome;
+            private int salario;
+
+            public Funcionario(string nome, int salario)
+            {
+                this.nome = nome;
+                this.salario = salario;
+
+                Console.WriteLine("O nome do funcionário é " + nome + " e recebe " + salario + " reais");
+
+
+            }
+
+            public Funcionario(string v, string nome, int salario)
+            {
+                this.v = v;
+                this.nome = nome;
+                this.salario = salario;
+            }
+            static void Main(string[] args)
+            {
+                string nome;
+                int salario;
+
+                Console.WriteLine("Digite o nome do funcionário: "); nome = Console.ReadLine();
+                Console.WriteLine("Digite o salário do funcionário: "); salario = int.Parse(Console.ReadLine());
+
+                Funcionario fun = new Funcionario(nome, salario);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
